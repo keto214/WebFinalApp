088b625039c926486445b613bda7fbb850cd2c85
keto214 <74033733+keto214@users.noreply.github.com>
2021-11-23 21:21:45 +0700

    Update NotFound.js

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -1,18 +1,18 @@
 import React from 'react';
-
+import "./notFound.css";
 class NotFound extends React.Component {
      render() {
           return (
                <div>
                     <section class="section-404">
                          <div class="text-404">
-                              <h1>404</h1>
-                              <h3>Opps! This page Could Not Be Found!</h3>
-                              <p>Sorry bit the page you are looking for does not exist, have been removed or name changed</p>
-                              <form action="#">
-                                   <input type="text" placeholder="Enter your keyword" />
-                                   <button type="submit"><i class="fa fa-search"></i></button>
-                              </form>
+                              <h2 class="word">Make by T.Cry Team</h2>
+                              <div class="namemem">
+                              <h2 class="word">Nguyễn Văn Châu - 4501104024</h2>
+                              <h2 class="word">Hồ Sỹ Chiến - 4501104027</h2>
+                              <h2 class="word">Nguyễn Minh Hiếu - 4501104083</h2>
+                              <h2 class="word">Lương Hoàng Quân - 4501104192</h2>
+                              </div>
                          </div>
                     </section>
                </div>
